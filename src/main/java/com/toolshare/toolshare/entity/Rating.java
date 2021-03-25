1c4ef05bf5c2b5bbcba2d23938b0ff395bda2658
Chris101099 <Christopher.Hoeflich@gmail.com>
2021-03-25 10:57:42 +0100

    updated Rating

--- src/main/java/com/toolshare/toolshare/entity/Rating.java
+++ src/main/java/com/toolshare/toolshare/entity/Rating.java
@@ -8,22 +8,17 @@ import org.springframework.hateoas.Link;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "ratings",
-        uniqueConstraints = {
-            @UniqueConstraint(columnNames = "title")
-        })
+@Table(name = "ratings")
 
 public class Rating {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private Link user_id;
-    private Link loan_id;
-    private Link tool_id;
 
     @NotBlank
     @Size(max = 50)
@@ -33,45 +28,51 @@ public class Rating {
     @Size(max = 300)
     private String text;
 
-    @Size(max = +1)
     private Integer ratingUp;
 
-    @Size(max = -1)
     private Integer ratingDown;
 
-    public Rating(String title, String text, Integer ratingUp, Integer ratingDown) {
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user_id;
+
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "tool_id", referencedColumnName = "id")
+    private Tool tool_id;
+
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "loan_id", referencedColumnName = "id")
+    private Loan loan_id;
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                ", ratingUp=" + ratingUp + '\'' +
+                ", ratingDown=" + ratingDown + '\'' +
+                ", user_id=" + user_id + '\'' +
+                ", tool_id=" + tool_id + '\'' +
+                ", loan_id=" + loan_id + '\'' +
+                '}';
+    }
+
+    public Rating(String title, String text, Integer ratingUp, Integer ratingDown, User user_id, Tool tool_id, Loan loan_id) {
         this.title = title;
         this.text = text;
         this.ratingUp = ratingUp;
         this.ratingDown = ratingDown;
+        this.user_id = user_id;
+        this.tool_id = tool_id;
+        this.loan_id = loan_id;
     }
 
     public Integer getId() {
         return id;
-    }
-
-    public Link getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Link user_id) {
-        this.user_id = user_id;
-    }
-
-    public Link getLoan_id() {
-        return loan_id;
-    }
-
-    public void setLoan_id(Link loan_id) {
-        this.loan_id = loan_id;
-    }
-
-    public Link getTool_id() {
-        return tool_id;
-    }
-
-    public void setTool_id(Link tool_id) {
-        this.tool_id = tool_id;
     }
 
     public void setId(Integer id) {
@@ -108,5 +109,29 @@ public class Rating {
 
     public void setRatingDown(Integer ratingDown) {
         this.ratingDown = ratingDown;
+    }
+
+    public User getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(User user_id) {
+        this.user_id = user_id;
+    }
+
+    public Tool getTool_id() {
+        return tool_id;
+    }
+
+    public void setTool_id(Tool tool_id) {
+        this.tool_id = tool_id;
+    }
+
+    public Loan getLoan_id() {
+        return loan_id;
+    }
+
+    public void setLoan_id(Loan loan_id) {
+        this.loan_id = loan_id;
     }
 }
