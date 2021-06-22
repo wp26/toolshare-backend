2f7f649284d128a50db98182a7cf13af7f3043b0
wp26 <berghaeuserpaul@gmail.com>
2021-06-22 10:29:50 +0200

    Update User.java
    Fixed User

--- src/main/java/com/toolshare/toolshare/entity/User.java
+++ src/main/java/com/toolshare/toolshare/entity/User.java
@@ -61,6 +61,12 @@ public class User {
 	public User() {
 	}
 
+	public User(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
 	public User(String username, String name, String surname, String email, String password) {
 		this.username = username;
 		this.name = name;
